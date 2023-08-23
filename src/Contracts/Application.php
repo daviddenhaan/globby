20c3d42c3f7fd1286efa6a8d58daaf3c5aff8c50
dulkoss <75451291+dulkoss@users.noreply.github.com>
2023-08-23 12:09:51 +0200

    Remove routing responsibility from Application

--- src/Contracts/Application.php
+++ src/Contracts/Application.php
@@ -2,15 +2,9 @@
 
 namespace Globby\Contracts;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 interface Application extends RequestHandlerInterface
 {
-    public function route(callable $route): static;
-
-    public function getHandler(string $uri, string $method): callable;
-
-    public function handle(ServerRequestInterface $request): ResponseInterface;
+    //
 }
