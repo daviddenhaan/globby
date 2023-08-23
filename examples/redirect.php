4fdc5a2cae531f371a9b6db8fa5f073d5bf5f44e
dulkoss <75451291+dulkoss@users.noreply.github.com>
2023-08-23 18:57:53 +0200

    add redirect example

---
+++ examples/redirect.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Globby\Method;
+use Globby\Route;
+use Globby\Router;
+use OpenSwoole\Http\Server;
+
+#[Route('/', Method::GET)]
+function hello()
+{
+    return Redirect('/redirected');
+}
+
+#[Route('/redirected')]
+function redirected()
+{
+    return Html("You got redirected!");
+}
+
+$application = (new Router)
+    ->route(hello::class)
+    ->route(redirected::class);
+
+$server = new Server('127.0.0.1', 8080);
+
+$server->setHandler($application);
+
+$server->start();
