51d4ec38502e37a12fcde0ee20577de3d27ddeb7
dulkoss <75451291+dulkoss@users.noreply.github.com>
2023-08-23 14:46:12 +0200

    make example for scoped middleware

---
+++ examples/scoped_middleware.php
@@ -0,0 +1,43 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Globby\Contracts\Middleware;
+use Globby\Contracts\Stack;
+use Globby\Route;
+use Globby\Router;
+use OpenSwoole\Http\Server;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+#[Route('/')]
+function root()
+{
+    return Html('TestMiddleware is not applied here.');
+}
+
+#[Route('/scoped')]
+function scoped()
+{
+    return Html('TestMiddleware is applied here!');
+}
+
+class TestMiddleware implements Middleware
+{
+    public function handle(ServerRequestInterface $request, Stack $stack): ResponseInterface
+    {
+        echo "It works!!";
+
+        return $stack->next($request);
+    }
+}
+
+$application = (new Router)
+    ->route(root::class)
+    ->route(scoped::class, [new TestMiddleware]);
+
+$server = new Server('127.0.0.1', 8080);
+
+$server->setHandler($application);
+
+$server->start();
